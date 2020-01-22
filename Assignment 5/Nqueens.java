8e09e54e80a49d2db24debacc4a1bf9af71f5382
Abhijeet Bhutia <abhijeet.bhutia@metacube.com>
2020-01-22 13:00:43 +0530

    Updated Nqueens

--- Assignment 5/Nqueens.java
+++ Assignment 5/Nqueens.java
@@ -1,71 +1,69 @@
-import java.util.Scanner;
-class Nqueens
-{
-	static boolean placeQueen(int[][] chess,int column,int n)
-	{
-		if(column==n)
-			return true;
-		for(int j=0;j<n;j++)
-		{
-			if(isSafe(chess,column,j,n))
-			{
-				chess[column][j]=1;
-				if(placeQueen(chess,column+1,n))
-					return true;
-				else
-				{
-					chess[column][j]=0;
-					continue;
-				}
-			}
-		}
-		return false;
-
+package hello;
+public class NQueen{ 
+    void printSolution(int board[][],int dimensionOfMatrix) 
+    { 
+        for (int i = 0; i < dimensionOfMatrix; i++) { 
+            for (int j = 0; j < dimensionOfMatrix; j++) 
+                System.out.print(" " + board[i][j] 
+                                 + " "); 
+            System.out.println(); 
+        } 
+    } 
+  
+    boolean isSafe(int board[][], int row, int col,int dimensionOfMatrix) 
+    { 
+        int i, j; 
+  
+        /* Check this row on left side */
+        for (i = 0; i < col; i++) 
+            if (board[row][i] == 1) 
+                return false; 
+  
+        /* Check upper diagonal on left side */
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+            if (board[i][j] == 1) 
+                return false; 
+  
+        /* Check lower diagonal on left side */
+        for (i = row, j = col; j >= 0 && i < dimensionOfMatrix; i++, j--) 
+            if (board[i][j] == 1) 
+                return false; 
+  
+        return true; 
+    } 
+  
+    boolean nQueen(int board[][], int startRow,int dimensionOfMatrix) 
+    { 
+        if (startRow >= dimensionOfMatrix) {
+            return true; 
+        }
+        for (int i = 0; i < dimensionOfMatrix; i++) { 
+            if (isSafe(board, i, startRow,dimensionOfMatrix)) { 
+                board[i][startRow] = 1; 
+  
+                if (nQueen(board, startRow + 1,dimensionOfMatrix) == true){ 
+                    return true; 
+                }
+                board[i][startRow] = 0;
+            } 
+        } 
+        return false; 
+    } 
+    
+  
+    public static void main(String args[]) 
+    { 
+    	NQueen Queen = new NQueen(); 
+    	int board[][] = { { 0, 0, 0, 0 }, 
+                { 0, 0, 0, 0 }, 
+                { 0, 0, 0, 0 }, 
+                { 0, 0, 0, 0 } }; 
+    	int dimensionOfMatrix=4;
+	if (Queen.nQueen(board, 0,dimensionOfMatrix) == false) { 
+	  System.out.print("Solution does not exist"); 
+	} 
+	else{
+		Queen.printSolution(board,dimensionOfMatrix); 
 	}
-	static boolean isSafe(int[][] q,int x,int y,int n)
-	{
-		for(int i=0;i<n;i++)
-		{
-			if(q[i][y]==1)
-				return false;
-			if(q[x][i]==1)
-				return false;
-		}
-		for(int i=0; x+i<n && y+i<n ;i++)
-		{
-			if(q[x+i][y+i]==1)
-				return false;
-		}
-		for(int i=0;x-i >= 0 && y-i >=0;i++)
-		{
-			if(q[x-i][y-i]==1)
-				return false;
-		}
-		for(int i=0;x+i < n && y-i >=0;i++)
-		{
-			if(q[x+i][y-i]==1)
-				return false;
-		}
-		for(int i=0;x-i >= 0 && y+i <n;i++)
-		{
-			if(q[x-i][y+i]==1)
-				return false;
-		}
-		return true;
-	}
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[][] chess=new int[n][n];
-		boolean b=Nqueens.placeQueen(chess,0,n);
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				System.out.print(" "+chess[i][j]);
-			}
-			System.out.println("");
-		} 
-	}
+    } 
 }
