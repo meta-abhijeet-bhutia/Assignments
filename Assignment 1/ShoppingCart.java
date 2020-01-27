e100caa48041f98c411d0498582fc4c3ef219bb6
meta-abhijeet-bhutia <59637098+meta-abhijeet-bhutia@users.noreply.github.com>
2020-01-27 10:42:51 +0530

    Update ShoppingCart.java
    updated

--- Assignment 1/ShoppingCart.java
+++ Assignment 1/ShoppingCart.java
@@ -1,80 +1,103 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap; 
 import java.util.Map; 
 
-class Item
+class ShoppingCart
 	{
-		public static HashMap<String, Integer> map1 = new HashMap<>(); //Item Name and their Price
-		Item()
-		{
-			map1.put("Vegetables",100);
-			map1.put("Fruits",200);
-			map1.put("Furniture", 500);
-		}
-	}
-class ShoppingCart extends Item
-	{
+		public static HashMap<String, Integer> itemNameAndPrice = new HashMap<>(); //Item Name and their Price
 		ShoppingCart()
 		{
-			super();	//Base Class Constructor
+			itemNameAndPrice.put("Silk",100);
+			itemNameAndPrice.put("Perk",50);
+			itemNameAndPrice.put("KitKat", 30);
 		}
 		public static BufferedReader br =new BufferedReader(new InputStreamReader(System.in)); 
 		public static HashMap<String, Integer> cart = new HashMap<>(); // Item Name and Quantity
 		public void Add_Item() throws IOException
 		{
-			System.out.println(map1);
-			System.out.println("Enter the item name you wish to add");
-			String Item_Name=br.readLine();
-			if(map1.containsKey(Item_Name))
-			{
-				System.out.println("Enter the quantity");
-				int Item_Quantity=Integer.parseInt(br.readLine());
-				cart.put(Item_Name,Item_Quantity);
-			}
-			else
-			{
-				System.out.println("We don`t have this item currently");
-			}
-			System.out.println("Do you wish to add more items");
-			String choice=br.readLine(); 
-			if(choice.equals("yes"))
-			{
-				Add_Item();
+			String choice="Y";
+			while(true)
+			{ 
+				if(choice.equalsIgnoreCase("Y"))
+				{
+					System.out.println("Item List:");
+					System.out.println("Items\tPrice");
+					for (Map.Entry<String,Integer> entry : itemNameAndPrice.entrySet())
+					{ 
+						if(entry.getValue()>0)
+						{
+							System.out.println(""+entry.getKey()+"\t"+entry.getValue());
+						}
+					}
+					System.out.println("Enter the item name you wish to add");
+					String Item_Name=br.readLine();
+					if(itemNameAndPrice.containsKey(Item_Name))
+					{
+						System.out.println("Enter the quantity");
+						int Item_Quantity=Integer.parseInt(br.readLine());
+						cart.put(Item_Name,Item_Quantity);
+					}
+					else
+					{
+						System.out.println("We don`t have this item currently");
+					}
+					System.out.println("Do you wish to add more items(Y/N)");
+					choice=br.readLine();
+				}
+				else if(choice.equalsIgnoreCase("N"))
+				{
+					break;
+				}
+				else
+				{
+					System.out.println("Wrong Choice");
+				}
 			}
 	}
 	public void Update_Item() throws IOException
 	{
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter the item you want to update");
-		String name=br.readLine();
-		System.out.println("Enter the updated quantity");
-		int temp=Integer.parseInt(br.readLine());
-		if (cart.containsKey(name))
-		{
-			cart.put(name,temp);
-		}
-		System.out.println("Do you wish to update more items");
-		String choice=br.readLine();
-		if(choice.equals("yes"))
-		{
-			Update_Item();
-		}
+		String choice="Y";
+			while(true)
+			{
+				System.out.println("Do you wish to update more items(Y/N)");
+				choice=br.readLine(); 
+				if(choice.equalsIgnoreCase("Y"))
+				{
+					System.out.println("Enter the item you want to update");
+					String name=br.readLine();
+					System.out.println("Enter the updated quantity");
+					int temp=Integer.parseInt(br.readLine());
+					if (cart.containsKey(name))
+					{
+						cart.put(name,temp);
+					}
+				}
+				else if(choice.equalsIgnoreCase("N"))
+				{
+					break;
+				}
+				else
+				{
+					System.out.println("Wrong Choice");
+				}
+			}
 	}
 	public void Display_Item()
 	{
 		int amount=0;
-		for (Map.Entry<String,Integer> entry : cart.entrySet())
-		{ 
-			if(entry.getValue()>0)
-			{
-				System.out.println("Item name = " + entry.getKey() + 
-                             ", Item Quantity = " + entry.getValue() + 
-                             ", Total Price = "+ map1.get(entry.getKey())*entry.getValue()); 
-				amount+=map1.get(entry.getKey())*entry.getValue();
+		System.out.println("Items\tQuantity\tTotal Price");
+			for (Map.Entry<String,Integer> entry : cart.entrySet())
+			{ 
+				if(entry.getValue()>0)
+				{
+					System.out.println(""+entry.getKey()+"\t"+entry.getValue()+"\t\t"+itemNameAndPrice.get(entry.getKey())*entry.getValue());
+					amount+=itemNameAndPrice.get(entry.getKey())*entry.getValue();
+				}
 			}
-		}
-		System.out.println("Total Shopping Cart Amount = " +amount);
+			System.out.println("Total Shopping Cart Amount = " +amount);
 	}
 	public static void main(String args[])throws IOException
 	{
